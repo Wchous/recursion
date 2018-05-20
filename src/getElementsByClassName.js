c50240fee810cf88d50fb8a891012c8ad5f80aa2
William Houstoun <wchoustoun@gmail.com>
2018-05-20 18:33:40 -0500

    completed basic requirements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var output = [];
+  var body = document.body;
+
+  var recursive = function (elem){
+  	if (elem.classList && elem.classList.contains(className)){
+  		output.push(elem)
+  	}
+  	if (elem.childNodes){
+  		for (var item of elem.childNodes){
+  			recursive(item)
+  		}
+  	}
+  }
+  recursive(body)
+  return output
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null){
+  	return 'null'
+  };
+  if (obj === undefined){
+  	return 'undefined'
+  };
+  if (typeof obj === 'string'){
+  	return '"' + obj + '"'
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+  	return String(obj)
+  };
+  if(Array.isArray(obj)){
+  	var output = ''
+  	for (var i=0; i<obj.length-1;i++){
+  		output += stringifyJSON(obj[i])+','
+  	}
+  	var lastElem = obj[obj.length-1] ? stringifyJSON(obj[obj.length-1]):'';
+  	return '[' + output + lastElem + ']'
+  }
+  if (typeof obj === 'object'){
+  	var objOutput = ''
+  	for (var keys in obj){
+  		if (keys !== 'undefined' && keys !== 'functions'){
+  			objOutput += stringifyJSON(keys)+':'+stringifyJSON(obj[keys]) + ',';
+  		}
+  	}
+  	return '{'+objOutput.substring(0,objOutput.length-1)+'}'
+  }
 };
